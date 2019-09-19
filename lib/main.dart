9c5c9d59593820b221b3184d8f4ed59059f8cc1e
pangtong <pangtong@98kplay.com>
2019-09-19 15:19:25 +0800

    启动页优化

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_craft/view/page/splash_page.dart';
 import 'package:flutter/services.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:auto_orientation/auto_orientation.dart';
 
-void main() async {
-  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-  await SystemChrome.setEnabledSystemUIOverlays([]);
-
-  runApp(MyApp());
+void main() {
+  Observable.fromFuture(
+          SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]))
+      .asyncMap((_) => SystemChrome.setEnabledSystemUIOverlays([]))
+      .asyncMap((_) => AutoOrientation.portraitUpMode())
+      .listen((_) => runApp(CraftApp()));
 }
 
-class MyApp extends StatelessWidget {
+class CraftApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
